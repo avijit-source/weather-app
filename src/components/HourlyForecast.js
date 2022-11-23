7beda4cba507cb2a69967eb8b2dc1ae9b8a01ae9
Avijit Chakraborty <avijitch123@outlook.com>
2022-11-23 23:06:18 +0530

    sec

--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -5,7 +5,7 @@ import Pagination from 'react-bootstrap/Pagination';
 
 function HourlyForecast({ hourlyForecast }) {
 
-    const [pages] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
+    const [pages] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
 
     const [currPage, setCurrPage] = useState(1);
 
